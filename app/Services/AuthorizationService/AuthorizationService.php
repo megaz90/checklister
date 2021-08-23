243bf919e74e75cc58d6d3196674d89ab173ace0
megaz90 <furqanamjad150@gmail.com>
2021-08-23 22:49:34 +0500

    Services Created

---
+++ app/Services/AuthorizationService/AuthorizationService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services\AuthorizationService;
+
+use App\Models\User;
+
+class AuthorizationService
+{
+    /**
+     * @param Model Instance 
+     * @param String $type (Implemented Permission)
+     * @return boolean
+     */
+
+    protected function check($model, string $type)
+    {
+        $user = auth()->user();
+        $data = User::with('roles.permissions.implemented_permission')->find($user->id);
+        if ($user->is_super_admin == TRUE) {
+            return true;
+        }
+
+        foreach ($data->roles as $role) {
+            foreach ($role->permissions as $permission) {
+                if ($permission->implemented_permission == null) {
+                    return false;
+                }
+                if ($permission->implemented_permission->name === $type) {
+                    return true;
+                }
+            }
+        }
+    }
+}
---
+++ app/Services/TaskService/TaskService.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Services\TaskService;
+
+class TaskService
+{
+}
