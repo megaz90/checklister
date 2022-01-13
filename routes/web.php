65b751bda987a433f10a5d91f02c9aca05f46826
Muhammad Furqan <furqanamjad150@gmail.com>
2022-01-13 21:07:14 +0500

    Checking

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ use App\Http\Controllers\User\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
